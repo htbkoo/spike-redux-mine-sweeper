a5fedc0cee873b218ef8e29132873d7c787d16c6
htbkoo <h1017760@connect.hku.hk>
2019-06-20 23:27:28 +0800

    added more tests

--- src/engine/redux/snake/services/SequenceGenerator.test.ts
+++ src/engine/redux/snake/services/SequenceGenerator.test.ts
@@ -2,19 +2,28 @@ import {RandomIntegerSequenceGenerator} from "./SequenceGenerator";
 
 describe('SequenceGenerator', function () {
     describe('RandomIntegerSequenceGenerator', () => {
-        it('should generate sequence ', () => {
-            // given
-            const source = randomGenerator();
-            const generator = new RandomIntegerSequenceGenerator({
-                random: () => source.next().value
-            });
+        [
+            {config: {start: 0, end: 10, length: 0}, expected: []},
+            {config: {start: 0, end: 10, length: 1}, expected: [2,]},
+            {config: {start: 0, end: 10, length: 2}, expected: [2, 4,]},
+            {config: {start: 0, end: 10, length: 3}, expected: [2, 4, 7]},
+            {config: {start: 0, end: 3, length: 3}, expected: [0, 1, 2,]},
+            {config: {start: 0, end: 4, length: 4}, expected: [0, 2, 3, 1]},
+        ].forEach(({config, expected}) =>
+            it(`should generate sequence for ${JSON.stringify(config)}`, () => {
+                // given
+                const source = randomGenerator();
+                const generator = new RandomIntegerSequenceGenerator({
+                    random: () => source.next().value
+                });
 
-            // when
-            const numbers = generator.generate({start: 0, end: 10, length: 3});
+                // when
+                const numbers = generator.generate(config);
 
-            // then
-            return expect(numbers).toEqual([2, 4, 7]);
-        });
+                // then
+                return expect(numbers).toEqual(expected);
+            })
+        );
 
         it('should generate distinct sequence even if random numbers are the same', () => {
             // given
@@ -37,7 +46,11 @@ describe('SequenceGenerator', function () {
 
             // when
             // then
-            return expect(() => generator.generate({start: 0, end: 10, length: 11})).toThrow("Invalid length, expected at most <10> but got <11>");
+            return expect(() => generator.generate({
+                start: 0,
+                end: 10,
+                length: 11
+            })).toThrow("Invalid length, expected at most <10> but got <11>");
         });
     });
 

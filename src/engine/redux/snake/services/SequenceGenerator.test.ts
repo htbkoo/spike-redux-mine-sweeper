7331b64a609ac7602703e8d95a4cbe9be8771995
htbkoo <h1017760@connect.hku.hk>
2019-06-20 15:10:38 +0800

    implemented SequenceGenerator.ts

---
+++ src/engine/redux/snake/services/SequenceGenerator.test.ts
@@ -0,0 +1,27 @@
+import {RandomIntegerSequenceGenerator} from "./SequenceGenerator";
+
+describe('SequenceGenerator', function () {
+    describe('RandomIntegerSequenceGenerator', () => {
+        it('should generate sequence ', () => {
+            // given
+            const source = randomGenerator();
+            const generator = new RandomIntegerSequenceGenerator({
+                random: () => source.next().value
+            });
+
+            // when
+            const numbers = generator.generate({start: 0, end: 10, length: 3});
+
+            // then
+            return expect(numbers).toEqual([2, 4, 7]);
+        });
+    });
+
+    function* randomGenerator() {
+        yield 0.23964912334176103;
+        yield 0.4221318771794387;
+        yield 0.7317617458221114;
+        yield 0.9503132062240779;
+        return;
+    }
+});
--- src/engine/redux/snake/services/SequenceGenerator.ts
+++ src/engine/redux/snake/services/SequenceGenerator.ts
@@ -1,9 +1,30 @@
+import _ from "lodash";
+
 export interface SequenceGenerator<T> {
     generate(config: { start: number, end: number, length: number }): Array<T>
 }
 
 export class RandomIntegerSequenceGenerator implements SequenceGenerator<number> {
-    generate(config: { start: number; end: number; length: number }): Array<number> {
-        throw new Error("Not implemented");
+    public static readonly DEFAULT: RandomIntegerSequenceGenerator = new RandomIntegerSequenceGenerator({
+        random: Math.random
+    });
+
+    private readonly random: () => number;
+
+    constructor({random}: { random: () => number }) {
+        this.random = random;
+    }
+
+    generate({start, end, length,}: { start: number; end: number; length: number }): Array<number> {
+        const candidates = _.range(start, end);
+
+        return _.range(0, length).map(() => candidates.splice(this.getRandomInt(0, candidates.length), 1)[0]);
+    }
+
+    // reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values
+    private getRandomInt(min: number, max: number) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(this.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
     }
 }

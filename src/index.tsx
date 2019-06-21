2263c6ec01bf67e64d1bf528f9a945de028f1b64
htbkoo <h1017760@connect.hku.hk>
2019-06-21 18:55:52 +0800

    updated formatting

--- src/index.tsx
+++ src/index.tsx
@@ -14,8 +14,8 @@ const store = configureStore({reducer: rootReducer,});
 ReactDOM.render(
     <Provider store={store}>
         <App/>
-    </Provider>
-    , document.getElementById('root')
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change

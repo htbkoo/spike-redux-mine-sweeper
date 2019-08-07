ab29719f8b1292074f8c390d6290aa0310caf22c
htbkoo <h1017760@connect.hku.hk>
2019-08-07 16:23:40 +0800

    built `/docs/`

---
+++ docs/precache-manifest.e448b8a259af7687750a20629a291aba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bb2a58ef9f3bbafcb8a7817e04ac7b",
+    "url": "/spike-redux-mine-sweeper/index.html"
+  },
+  {
+    "revision": "c412aca3953e98ec2af4",
+    "url": "/spike-redux-mine-sweeper/static/css/main.697e29ad.chunk.css"
+  },
+  {
+    "revision": "0ecd83eebc04e76adf5c",
+    "url": "/spike-redux-mine-sweeper/static/js/2.aa96c7e4.chunk.js"
+  },
+  {
+    "revision": "c412aca3953e98ec2af4",
+    "url": "/spike-redux-mine-sweeper/static/js/main.5c7f8961.chunk.js"
+  },
+  {
+    "revision": "3dd10cf1b378d06bf195",
+    "url": "/spike-redux-mine-sweeper/static/js/runtime~main.45b1143f.js"
+  }
+]);
---
+++ docs/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/spike-redux-mine-sweeper/precache-manifest.e448b8a259af7687750a20629a291aba.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spike-redux-mine-sweeper/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});

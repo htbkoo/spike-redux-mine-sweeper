e8f3cdd22502fa8278fcbea51dc85d81b44ac8f5
htbkoo <h1017760@connect.hku.hk>
2019-08-07 19:10:05 +0800

    added a selector `getBoardSize` as practice

--- src/engine/redux/snake/game/selectors.test.ts
+++ src/engine/redux/snake/game/selectors.test.ts
@@ -1,8 +1,9 @@
-import {getGameState} from "./selectors";
+import {getBoardSize, getGameState} from "./selectors";
 import {DEFAULT_BOARD} from "../models/Board";
+import {GameState} from "../models/state";
 
 describe("selectors", function () {
-    it("should ", () => {
+    it("should getGameState", () => {
         // given
         const gameState = {
             config: {
@@ -23,5 +24,21 @@ describe("selectors", function () {
 
         // then
         expect(actual).toEqual(gameState);
+    });
+
+    it("should get board size", () => {
+        // given
+        const gameState: GameState = {
+            board: DEFAULT_BOARD,
+        } as any;
+
+        // when
+        const actual = getBoardSize({
+            game: gameState
+        });
+
+        // then
+        const expected = {h: 8, w: 8};
+        expect(actual).toEqual(expected);
     });
 });
--- src/engine/redux/snake/game/selectors.ts
+++ src/engine/redux/snake/game/selectors.ts
@@ -1,7 +1,18 @@
 // TODO: leverage reselect and re-reselect
-// import { createSelector } from 'reselect';
+import {createSelector} from 'reselect';
 
-// import { TodosState } from './reducer';
 import {AppState, GameState} from "../models/state";
 
 export const getGameState = (state: AppState) => state.game;
+export const getBoardSize = createSelector(
+    getGameState,
+    extractBoardSize
+);
+
+type BoardSize = { h: number, w: number }
+
+function extractBoardSize({board: {cells}}: GameState): BoardSize {
+    const h = cells ? cells.length : 0;
+    const w = (cells && h > 0) ? cells[0].length : 0;
+    return {h, w};
+}
